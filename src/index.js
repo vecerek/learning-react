97a9326507f93d56bb8dc8751638c8736c4f4a54
Attila Večerek <attila.vecerek@gmail.com>
2017-11-03 13:53:20 +0100

    Renders board using 2 loops

--- src/index.js
+++ src/index.js
@@ -19,23 +19,28 @@ class Board extends React.Component {
   }
 
   render() {
+    const board = [0, 1, 2].map((i) => {
+      return (
+        <div className="board-row" key={"row-" + i}>
+          {
+            [0, 1, 2].map((j) => {
+              let val = i*3 + j;
+              return (
+                <Square 
+                  key={"col-" + val}
+                  value={this.props.squares[val]}
+                  onClick={() => this.props.onClick(val)}
+                />
+              );
+            })
+          }
+        </div>
+      );
+    });
+
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {board}
       </div>
     );
   }
